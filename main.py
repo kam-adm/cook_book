aaf66a18e68ba0c628891ee3277e39ad50b91cb7
Troll <kam-adm@mail.ru>
2023-03-14 19:03:12 +0300

    task2

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from pprint import pprint
-with open('recipes.txt', 'rt') as file:
+
+
+with open('recipes.txt', 'rt', encoding='utf-8') as file:
     cook_book = {}
-    s = cook_book
     for line in file:
         dish_name = line.strip()
         quantity_ingredients = int(file.readline())
         list_ingredients = []
-        print(dish_name)
         for _ in range(quantity_ingredients):
-            ingredient_name, quantity, measure = file.readline().strip().split(' | ')
+            ingredient_name, measure, quantity = file.readline().strip().split(' | ')
             list_ingredients.append({
-             'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure
-            })
+                'ingredient_name': ingredient_name,
+                'measure': int(measure),
+                'quantity': quantity
+                 })
         file.readline()
         cook_book[dish_name] = list_ingredients
-        print(list_ingredients)
-    #pprint(cook_book, sort_dicts=False)
+
+
+
+    def get_shop_list_by_dishes(dishes, person_count):
+        ingredient_list_by_dishes = {}
+        for i in cook_book:
+            for m in dishes:
+                if i == m:
+                    for n in cook_book[i]:
+                        if n['ingredient_name'] not in ingredient_list_by_dishes:
+                            ingredient_list_by_dishes.setdefault(n['ingredient_name'], []).append(n['measure'] * person_count)
+                            ingredient_list_by_dishes.setdefault(n['ingredient_name'], []).append(n['quantity'])
+                        else:
+                            ingredient_list_by_dishes[ingredient_name][0] += ingredient_list_by_dishes[ingredient_name][0]
+
+        pprint(ingredient_list_by_dishes)
+
+    get_shop_list_by_dishes(['Омлет', 'Запеченный картофель', 'Фахитос'], 1)
